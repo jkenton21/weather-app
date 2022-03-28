39e874178389ca2f916291752de2f9ffc0b6c2ff
jkenton21 <jkenton21@gmail.com>
2022-03-28 16:45:26 -0500

    initial skeleton files added

---
+++ src/index.js
@@ -0,0 +1,1 @@
+//Weather Application JS Index File (import modules)
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+//Webpack config file
+
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
